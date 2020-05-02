23a2b228789ffd3c7a5a1acec2aa3f3378bc91b3
hexu <2212925883@qq.com>
2020-05-02 14:04:28 +0800

    ebank-springcloud demo

---
+++ ebank-springintegration/src/main/java/com/hexu/ebank/springintegration/EbankSpringintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.hexu.ebank.springintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankSpringintegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankSpringintegrationApplication.class, args);
+    }
+
+}
---
+++ ebank-springintegration/src/test/java/com/hexu/ebank/springintegration/EbankSpringintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexu.ebank.springintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankSpringintegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
